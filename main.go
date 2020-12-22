44b1be55a8b94d5fc6b6c00ce4aac9518710918e
Donizete (Dimmy) Junior <dimmy.junior@mercadolivre.com>
2020-12-22 17:02:17 -0300

    DOJO TAXES BR

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-	hi := Hello("Jarvis")
-	fmt.Print(hi)
+
+	result := createHandler("^/taxes-gateway", "# Tax Collected Source Gateway",[] string {
+		createRule("", "", true),
+		createRule("http://stage.taxes-collected-source-gateway.melifrontends.com", "stage", false),
+		createRule("http://dev.taxes-collected-source-gateway.melifrontends.com", "dev", false),
+		createRule("http://prod.taxes-collected-source-gateway.melifrontends.com", "", false),
+	})
+	fmt.Printf(result)
+	err := ioutil.WriteFile("rules.conf", []byte(result), 0755)
+	if err != nil {
+		fmt.Printf("Unable to write file: %v", err)
+	}
+}
+
+func createHandler(path, commentBlock string, rules[] string) string {
+	proxyRulesString := commentBlock + "\n"
+	proxyRulesString += "location ~ "+ path + " {\n\t"
+	for i := 0; i < len(rules); i++ {
+		proxyRulesString += rules[i] + "\n\t"
+	}
+	proxyRulesString += "\n}"
+	return proxyRulesString
+}
+
+func createRule(host, scope string, public bool) string {
+	if public == true{
+		return "if ($http_x_public = \"true\") { return 403; break;} # NOT PUBLIC API \n"
+	}
+
+	if len(scope) > 0 {
+		return "if ($http_x_api_scope = \"" + scope + "\") {\n\t\tproxy_pass " + host + ";\n\t\tbreak;\n\t}\n"
+	}
+	return "proxy_pass " + host + ";"
 }
 
 
-func Hello(s string) string {
-	return "Hello " + s
-}
+
--- main_test.go
+++ main_test.go
@@ -1,14 +1,8 @@
 package main
 
 import "testing"
-import "fmt"
 
 func TestHello(t *testing.T) {
-	result := Hello( "Jarvis")
 
-	fmt.Println(result)
-	if result != "Hello Jarvis" {
-		t.Errorf("Deu ruim!!")
-	}
 
 }
