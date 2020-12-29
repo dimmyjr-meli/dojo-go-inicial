f3eb37cffed235b809f2b387cd6a1a9fc89a7f2d
Donizete (Dimmy) Junior <dimmy.junior@mercadolivre.com>
2020-12-29 16:12:20 -0300

    Dojo 29/12/2020 - AR

--- main.go
+++ main.go
@@ -1,13 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	hi := Hello("Jarvis")
-	fmt.Print(hi)
+	regras := []Regras{
+		Regras{
+			scope: "stage",
+			host:  "http://stage.tax-collected-source-gateway.melifrontends.com",
+		},
+		Regras{
+			scope: "dev",
+			host:  "http://dev.tax-collected-source-gateway.melifrontends.com",
+		},
+		Regras{
+			host: "http://prod.tax-collected-source-gateway.melifrontends.com",
+		},
+	}
+	Entrada ("Tax Collected Source Gateway"," ~ ^/taxes-gateway ",false, regras)
 }
 
 
 func Hello(s string) string {
 	return "Hello " + s
+}
+
+ type Regras struct {
+	scope string
+	host string
+ }
+
+func Entrada(comments string, location string, http_x_public bool, listRegras []Regras) {
+	fmt.Println("#   ", comments)
+	fmt.Printf("location %s {\n", location)
+
+	if (!http_x_public) {
+		fmt.Println("  if ($http_x_public = \"true\") { return 403; break;} # NOT PUBLIC API\n")
+	}
+	for _, val := range listRegras{
+		if val.scope != "" {
+			fmt.Printf("  if ($http_x_api_scope = \"%s\") {\n", val.scope)
+			fmt.Printf("    proxy pass %s; \n    break;\n", val.host)
+			fmt.Println("  }\n")
+		} else {
+				fmt.Printf("  proxy pass %s; \n", val.host)
+		}
+	}
+	fmt.Println("}")
+
+
+
 }
